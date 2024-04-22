40eced62118c743dc46c8ff2e7522f8ef4f9648b
Michael Kennedy <michael@michaelkennedy.dev>
2024-04-21 21:33:24 -0700

    Adding modified seed file for config uploading to both Firebase and MongoDB

--- src/seed.py
+++ src/seed.py
@@ -23,11 +23,13 @@ def populate(db, client):
         files = listFiles(path)
         for file in files:
             with open(file, "r") as inputFile:
-                file_name = getFileName(file)[:-5]
+                inputFile_json = json.load(inputFile)
                 if client == "MongoDB":
-                    seed_arr.append({"_id": file_name, **json.load(inputFile)})
+                    file_name = inputFile_json["name"]
+                    inputFile_json.pop('name', None)
+                    seed_arr.append({"_id": file_name, **inputFile_json})
                 else:
-                    seed_arr.append(json.load(inputFile))
+                    seed_arr.append(inputFile_json)
         try:
             match client:
                 case "MongoDB":
@@ -37,25 +39,24 @@ def populate(db, client):
                     batch = db.batch()
                     for seed in seed_arr:
                         doc_ref = db.collection(collection_prefix).document(seed["name"])
+                        seed.pop('name', None)
                         batch.set(doc_ref, seed)
                     batch.commit()
         except Exception as e:
             print(f'Failed to seed {client} with error {e}')
             return
-        
     print(f'Seeded {client}')
     
             
-def clear_db(db, client, suffix):
+def clear_db(db, client):
     for path, collection_prefix in paths.items():
-        collection_key = collection_prefix + suffix
         try:
             match client:
                 case "MongoDB":
-                    db[collection_key].delete_many({})
+                    db[collection_prefix].delete_many({})
                 case "Firebase":
                     batch = db.batch()
-                    collection = db.collection(collection_key)
+                    collection = db.collection(collection_prefix)
                     docs = collection.stream()
                     for doc in docs:
                         print(f"Deleting {doc.id}")
@@ -66,8 +67,26 @@ def clear_db(db, client, suffix):
              print(f'Failed to clear {client} with error {e}')
              return
         
-    print(f'Cleared {client} for {suffix}')
+    print(f'Cleared {client}')
 
+def test_db(db, database_selection, database_environment):
+    db = None
+    try:
+        match database_selection:
+            case "MongoDB":
+                import pymongo
+                client = pymongo.MongoClient("mongodb://%s:%s@%s" % (config["MONGODB_USER"], config["MONGODB_PASSWORD"], config["MONGODB_URL"]))
+                client["rentalreviews"]
+            case "Firebase":
+                import firebase_admin
+                from firebase_admin import credentials, firestore
+                cred = credentials.Certificate(server_certificates[database_environment])
+                firebase_admin.initialize_app(cred)
+                db = firestore.client()
+        
+        return {"status": True, "exception": None}
+    except Exception as e:
+        return {"status": False, "exception": e}
 
 
 def main(database_selection, database_action, database_environment):
@@ -90,21 +109,26 @@ def main(database_selection, database_action, database_environment):
         return
 
     match database_action:
+        case "Test":
+            ret = test_db(db, None, database_action)
+            if ret['status']:
+                print(f'Connection successfully initialized to {database_selection}')
+            else:
+                 print(f'Failed to initialize source {database_selection} with exception {ret["exception"]}')
         case "Seed":
-            populate(db, database_selection, "companies")
-            populate(db, database_selection, "properties")
+            populate(db, database_selection)
         case "Clear":
-            clear_db(db, database_selection, "companies")
-            clear_db(db, database_selection, "properties")
+            clear_db(db, database_selection)
+            clear_db(db, database_selection)
         case "Re-seed":
-            clear_db(db, database_selection, "companies")
-            populate(db, database_selection, "companies")
-            clear_db(db, database_selection, "properties")
-            populate(db, database_selection, "properties")
+            clear_db(db, database_selection)
+            populate(db, database_selection)
+            clear_db(db, database_selection)
+            populate(db, database_selection)
 
 if __name__ == "__main__":
     database_selection = pyinput.inputMenu(["MongoDB", "Firebase"], lettered=True, numbered=False)
-    database_action = pyinput.inputMenu(["Seed", "Clear", "Re-seed"], lettered=True, numbered=False)
+    database_action = pyinput.inputMenu(["Test", "Seed", "Clear", "Re-seed"], lettered=True, numbered=False)
     database_environment = ""
     if database_selection != "MongoDB":
         database_environment = pyinput.inputMenu(["Production", "Staging"], lettered=True, numbered=False)
